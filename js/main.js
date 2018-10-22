16301984d813132d32923c95b07e5134b8faa710
李桂炎 <partrick.li@gmail.com>
2018-10-22 11:22:09 +0800

    feat: Resize image file for responsize image configuration

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = DBHelper.imageUrlForRestaurant(restaurant, 'large');
   li.append(image);
 
   const name = document.createElement('h1');
