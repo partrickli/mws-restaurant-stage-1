c7721443f2ff335a9c7af3f604928fdd52183bec
李桂炎 <partrick.li@gmail.com>
2018-11-08 23:45:52 +0800

    fix: Service worker bug

--- sw.js
+++ sw.js
@@ -5,19 +5,12 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request).then(function(response) {
       // Cache hit - return response
       if (response) {
-        console.log(`serve by service worker: ${event.request.url}`);
         return response;
       }
 
       var fetchRequest = event.request.clone();
 
       return fetch(fetchRequest).then(function(response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          console.log(`serve by fetch: ${event.request.url}`);
-          return response;
-        }
-
         var responseToCache = response.clone();
 
         caches.open(CACHE_NAME).then(function(cache) {
