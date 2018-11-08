97908ebb21468f080065fcb02f48bbe5c7c4a3f5
李桂炎 <partrick.li@gmail.com>
2018-11-08 22:54:02 +0800

    fix: Set restaurant title to h2 for proper semantic

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ createRestaurantHTML = (restaurant, index) => {
   image.srcset = images.join(', ');
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
